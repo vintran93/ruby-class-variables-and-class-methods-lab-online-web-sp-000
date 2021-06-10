97637d4e7db177f3caf5043670541ac66ae22cb0
Vincent Tran <vincentuong@yahoo.com>
2021-06-10 04:05:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,20 +3,24 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@artists = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists << artist
+    @@genres << genre
   end
   
   def self.count
     @@count
   end
   
-  def self.genres
-    arr
+  def self.artists
+    @@genres.uniq
 end 
     
     
