8440296b6aee5d2f62dcfbe7d70e2d8274e1f8cd
Vincent Tran <vincentuong@yahoo.com>
2021-06-10 03:39:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,22 @@
-class Song
-  
-  attr_accessor = 
+class Artist
+	attr_accessor :name
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
+
+	def songs
+		@songs
+	end
+
+	def add_song(song)
+			@songs << song
+			song.artist = self
+	end
+
+	def genres
+		@songs.collect do |x|
+			x.genre
+		end
+	end
+end
