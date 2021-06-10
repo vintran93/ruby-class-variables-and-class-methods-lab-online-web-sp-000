6ca7be798e98fa9854fd0a0021742153ed4bedce
Vincent Tran <vincentuong@yahoo.com>
2021-06-10 04:16:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,6 +33,11 @@ class Song
     genre_count
   end 
   
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
+    artist_count
+  end
 end 
     
     
